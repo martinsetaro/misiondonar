a6e28d9df2ec56982b0b2ae31d71772d6d45182c
martin setaro <lordsesony@gmail.com>
2022-12-17 00:48:42 -0300

    arreglado envio de mail

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -7,9 +7,9 @@ const Formulario = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
-        const form = useRef();
+        
     
-        emailjs.sendForm('misiondonarmendoza', 'template_kamhvpd', form.current, 'nF0V99lOO5mXCUZlZY')
+        emailjs.sendForm('misiondonarmendoza', 'template_kamhvpd',e.target, 'nF0V99lOO5mXCUZlZY')
           .then((result) => {
               console.log(result.text);
           }, (error) => {
@@ -22,13 +22,13 @@ const Formulario = () => {
     <div className="contenedorFormulario">
         <h2>Formulario de contacto</h2>
         <p>Aqui podras ponerte en contacto con nosotros y decirnos que elemento puedes donar</p>
-        <form className='formulario' ref={form} onSubmit={sendEmail}>
+        <form className='formulario' onSubmit={sendEmail}>
             <label>Ingresa tu nombre</label>
             <input type="text" placeholder="Ingrese su nombre" name="user_name"/>
             <label>Ingresa tu apellido</label>
             <input type="text" placeholder="Ingrese su apellido"/>
             <label>Ingresa tu telefono</label>
-            <input type="number" placeholder="Ingrese su telefono" name="phone"/>
+            <input type="number" placeholder="Ingrese su telefono" name="user_phone"/>
             <label>Ingresa tu correo electronico</label>
             <input type="email" placeholder="Ingrese su email" name="user_email"/>
             <label>Selecciona tu departamento</label>
